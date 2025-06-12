5d82c240b2d38183fb1e8e738caf132ee7eb98d8
JorgeIvanGF_WSL <jorgeivangf@gmail.com>
2025-06-12 14:42:22 +0200

    CHORE(ex01): Destructor method added. Override Attack method added. guardGate method added.

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: jorgutie <jorgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 19:13:37 by jorgutie          #+#    #+#             */
-/*   Updated: 2025/06/11 19:51:37 by jorgutie         ###   ########.fr       */
+/*   Updated: 2025/06/12 14:40:56 by jorgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,29 @@ ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
 {
 	*this = other; // using the operator=
 	std::cout << "ScavTrap copy constructor called.\n";
+}
+
+// Destructor
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << YELLOW << _name << RESET << " destructed.\n";
+}
+
+// Override attack
+void ScavTrap::attack(const std::string& target)
+{
+	if (_hitPoints <= 0 || _energyPoints <= 0)
+	{
+		std::cout << "ScavTrap " << _name << " is too tired or dead to attack.\n";
+		return;
+	}
+	_energyPoints--;
+	std::cout << "ScavTrap " << YELLOW << _name << RESET << " fiercely attacks " << target
+			  << ", causing " << YELLOW << _attackDamage << RESET << " points of damage!\n";
+}
+
+// Unique method
+void ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap " << YELLOW << _name << RESET << " is now in Gate Keeper mode!\n";
 }
